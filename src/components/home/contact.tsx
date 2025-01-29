8a98d3b0d918f8816a7c87e3421a9129cc444202
Od4ir <od4ir@usp.br>
2025-01-29 17:58:03 -0300

    fix | Header style for selected buttons

--- src/components/home/contact.tsx
+++ src/components/home/contact.tsx
@@ -13,7 +13,7 @@ const ContactIcon = ({ src, alt, label }: { src: string, alt: string, label: str
 
 export const Contato = () => {
     return (
-        <div id='contato' className="h-[20rem] w-full justify-center items-center flex flex-col gap-4 bg-gradient-to-r from-tecs-primary to-tecs-secondary text-white">
+        <div id='contato' className="h-[50vh] w-full justify-center items-center flex flex-col gap-4 bg-gradient-to-r from-tecs-primary to-tecs-secondary text-white">
             <h3 className='text-[1.5rem] font-bold'>CONTATO</h3>
             <div className='flex justify-center items-center md:gap-4'>
                 <ContactIcon src="icones/insta.svg" alt="Ícone do Instagram" label="INSTAGRAM" />
--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,9 +1,12 @@
+"use client";
 import { MenuIcon } from "lucide-react"
 import Image from "next/image"
+import { useState, useEffect } from "react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 
-
 export const Header = () => {
+    const [activeSection, setActiveSection] = useState<string>('');
+
     const estilo_nav_item = "min-w-[312px] text-tecs-secondary hover:bg-gradient-to-r from-tecs-secondary to-tecs-primary hover:text-white my-2 font-bold py-2 text-[0.9rem] rounded-full ";
     const estilo_nav_item_desk = "hover:bg-tecs-alternative font-bold px-6 m-2 py-2 text-[1rem] rounded-[20px] transition-all";
 
@@ -14,6 +17,28 @@ export const Header = () => {
         { label: 'APOIADORES', href: '#apoio' },
         { label: 'CONTATO', href: '#contato' }
     ];
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY + window.innerHeight / 2;
+            const currentSection = bc_sections.find(section => {
+                const element = document.querySelector(section.href);
+                if (element) {
+                    const { offsetTop, offsetHeight } = element as HTMLElement;
+                    return scrollPosition >= offsetTop && scrollPosition < offsetTop + offsetHeight;
+                }
+                return false;
+            });
+            if (currentSection) {
+                setActiveSection(currentSection.href);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div className="fixed flex flex-row min-h-[120px] items-center justify-between w-screen min-w-[320px] w-screen lg:w-screen px-6 bg-gradient-to-r from-tecs-secondary to-tecs-primary z-30">
@@ -28,7 +53,7 @@ export const Header = () => {
                     <DropdownMenuContent className="flex mt-[2.5rem] justify-center items-center text-center min-w-[312px] border-[4px] border-white shadow-2xl rounded-lg">
                         <ul>
                             {bc_sections.map((section, index) => (
-                                <li key={index} className={estilo_nav_item}>
+                                <li key={index} className={`${estilo_nav_item} ${activeSection === section.href ? 'bg-tecs-alternative text-white' : ''}`}>
                                     <a href={section.href}>{section.label}</a>
                                 </li>
                             ))}
@@ -38,7 +63,7 @@ export const Header = () => {
             </div>
             <ul className="hidden lg:flex text-white flex-row items-center justify-center">
                 {bc_sections.map((section, index) => (
-                    <li key={index} className={estilo_nav_item_desk}>
+                    <li key={index} className={`${estilo_nav_item_desk} ${activeSection === section.href ? 'bg-tecs-alternative text-white' : ''}`}>
                         <a href={section.href}>{section.label}</a>
                     </li>
                 ))}
