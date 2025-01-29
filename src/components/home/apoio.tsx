31b725e99b648450535fee699311fb9626ae2f44
Od4ir <od4ir@usp.br>
2025-01-29 17:31:04 -0300

    style | Desktop version for contact and apoio components

--- src/components/home/apoio.tsx
+++ src/components/home/apoio.tsx
@@ -1,16 +1,16 @@
 export const Apoio = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white pt-[100px]">
+        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-white ">
             <h3 className="text-[2rem] font-bold text-tecs-secondary">APOIOS</h3>
             <p>Conheça nossos apoiadores:</p>
 
-            <div className="flex flex-col justify-center items-center sm:grid-cols-2 gap-[2.5rem] w-full px-6 py-6">
-                <img src="apoiadores/logo_fapesp.png" alt="Logo da FAPESP" className="w-[8rem]" />
-                <img src="apoiadores/logo_ime.png" alt="Logo do IME" className="w-[8rem]" />
-                <img src="apoiadores/logo_usp.png" alt="Logo da USP" className="w-[8rem]" />
-                <img src="apoiadores/logo_fundacao_casa.png" alt="Logo da Fundação CASA" className="w-[8rem]" />
-                <img src="apoiadores/logo_tech_shift.png" alt="Logo da Tech Shift" className="w-[8rem]" />
-                <img src="apoiadores/logo_jornal_usp.png" alt="Logo do Jornal da USP" className="w-[8rem]" />
+            <div className="flex flex-wrap justify-center items-center gap-[2.5rem] md:gap-[4rem] max-w-[35rem] px-6 py-6 md:py-[4rem]">
+                <img src="apoiadores/logo_fapesp.png" alt="Logo da FAPESP" className="w-[8rem] md:w-[12rem]" />
+                <img src="apoiadores/logo_ime.png" alt="Logo do IME" className="w-[8rem] md:w-[12rem]" />
+                <img src="apoiadores/logo_usp.png" alt="Logo da USP" className="w-[8rem] md:w-[12rem]" />
+                <img src="apoiadores/logo_fundacao_casa.png" alt="Logo da Fundação CASA" className="w-[8rem] md:w-[12rem]" />
+                <img src="apoiadores/logo_tech_shift.png" alt="Logo da Tech Shift" className="w-[8rem] md:w-[12rem]" />
+                <img src="apoiadores/logo_jornal_usp.png" alt="Logo do Jornal da USP" className="w-[8rem] md:w-[12rem]" />
             </div>
         </div>
     );
--- src/components/home/contact.tsx
+++ src/components/home/contact.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactIcon = ({ src, alt, label }: { src: string, alt: string, label: string }) => {
     return (
-        <div className="flex flex-col items-center justify-center w-[6rem] h-[5rem] text-[0.7rem] text-white font-bold gap-1">
+        <div className="flex flex-col items-center justify-center w-[6rem] h-[5rem] text-[0.7rem] text-white font-bold gap-1 hover:border hover:border-tecs-primary hover:border-solid hover:border-[2px] hover:rounded-lg hover:cursor-pointer">
             <div className="flex justify-center items-center bg-white w-[3rem] h-[3rem] rounded-full">
                 <img src={src} alt={alt} />
             </div>
@@ -15,7 +15,7 @@ export const Contato = () => {
     return (
         <div className="h-[20rem] w-full justify-center items-center flex flex-col gap-4 bg-gradient-to-r from-tecs-primary to-tecs-secondary text-white">
             <h3 className='text-[1.5rem] font-bold'>CONTATO</h3>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center md:gap-4'>
                 <ContactIcon src="icones/insta.svg" alt="Ícone do Instagram" label="INSTAGRAM" />
                 <ContactIcon src="icones/email.svg" alt="Ícone do Email" label="EMAIL" />
                 <ContactIcon src="icones/linkedin.svg" alt="Ícone do LinkedIn" label="LINKEDIN" />
